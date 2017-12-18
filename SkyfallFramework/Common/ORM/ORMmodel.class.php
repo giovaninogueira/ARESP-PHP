0dd0959413904f6d19e7dbedf8c151fedc10f8ba
Giovani Cassiano <giovani.cassiano@riosoft.com.br>
2017-12-18 14:36:04 -0200

    Corrigido ORM

---
+++ SkyfallFramework/Common/ORM/ORMmodel.class.php
@@ -0,0 +1,297 @@
+<?php
+
+namespace SkyfallFramework\Common\ORM;
+
+/**
+ * Class ORMmodel
+ * @package SkyfallFramework\Common\ORM
+ * @author Giovani Cassiano Nogueira <giovani.cassiano@outlook.com>
+ */
+class ORMmodel
+{
+    #region Atributos
+    private $use = "";
+    private $getSet = "";
+    private $atributos = "";
+    private $table;
+    private $type;
+    private $referencia;
+    private $model;
+    #endregion
+
+    #region Get e Sets
+    /**
+     * @param $use
+     */
+    public function setUse($use)
+    {
+        $this->use = $use;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUse()
+    {
+        return $this->use;
+    }
+
+    /**
+     * @param $getSet
+     */
+    public function setGetSet($getSet)
+    {
+        $this->getSet = $getSet;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGetSet()
+    {
+        return $this->getSet;
+    }
+
+    /**
+     * @param $atributos
+     */
+    public function setAtributos($atributos)
+    {
+        $this->atributos = $atributos;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAtributos()
+    {
+        return $this->atributos;
+    }
+
+    /**
+     * @param $table
+     */
+    public function setTable($table)
+    {
+        $this->table = $table;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    /**
+     * @param $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param $referencia
+     */
+    public function setReferencia($referencia)
+    {
+        $this->referencia = $referencia;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReferencia()
+    {
+        return $this->referencia;
+    }
+
+    /**
+     * @param $model
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+    #endregion das
+
+    /**
+     * @return string
+     * @details Retorna o texto para criar os arquivos de traits
+     */
+    #region CreateTrait
+    public function createFiles()
+    {
+        $php = "<?php\n\n";
+        $php .= "/**\n";
+        $php .= "\tSkyfall Micro-Framework\n";
+        $php .= "\tGenerate ORM Trait" . "\n";
+        $php .= "\tVersion 1.0.0\n";
+        $php .= "**/\n\n";
+        $php .= "namespace MVC\Traits; \n\n";
+        $php .= $this->use . "\n\n";
+        $php .=  "trait " . $this->table;
+        $php .= "\n{\n";
+        $php .= $this->atributos;
+        $php .= $this->getSet;
+        $php .= "\n}";
+        return $php;
+    }
+    #endregion
+
+    /**
+     * @param $table
+     * @return string
+     * @details Retorna o texto para criar os arquivos de models
+     */
+    #region CreateModel
+    public function createModel($table)
+    {
+        $php = "<?php\n\n";
+        $php .= "/**\n";
+        $php .= "\tSkyfall Micro-Framework\n";
+        $php .= "\tGenerate ORM Model" . "\n";
+        $php .= "\tVersion 1.0.0\n";
+        $php .= "**/\n\n";
+        $php .= "namespace MVC\Model; \n\n";
+        $php .= "class " . $table;
+        $php .= " extends Model";
+        $php .= "\n{";
+        $php .= $this->use;
+        $php .= "\n}";
+        return $php;
+    }
+    #endregion
+
+    /**
+     * @param $table
+     * @return string
+     * @details Retorna o texto para criar os arquivos de controller
+     */
+    #region CreateController
+    public function createController($table)
+    {
+        $php = "<?php\n\n";
+        $php .= "/**\n";
+        $php .= "\tSkyfall Micro-Framework\n";
+        $php .= "\tGenerate ORM Controller" . "\n";
+        $php .= "\tVersion 1.0.0\n";
+        $php .= "**/\n\n";
+        $php .= "namespace MVC\Controller; \n\n";
+        $php .= "class " . $table;
+        $php .= "\n{\n";
+        $php .= "/**\n";
+        $php .= "\tSkyfall Micro-Framework\n";
+        $php .= "\tController's body" . "\n";
+        $php .= "\tVersion 1.0.0\n";
+        $php .= "**/";
+        $php .= "\n}";
+        return $php;
+    }
+    #endregion
+
+    /**
+     * @param $referencia
+     * @param string $model
+     * @details Cria o set com referencia para objeto passando para o atributo getSet
+     */
+    #region CreateSetRef
+    public function createSetRef($referencia, $model = "")
+    {
+        $set = "\n\tpublic function set" . ucwords($referencia);
+        $set .= "(" . ucwords($referencia) . ucwords($model);
+        $set .= " $" . $referencia . ")\n";
+        $set .= "\t{\n";
+        $set .= "\t\t$" . "this->" . strtolower($referencia) . " = $" . $referencia . ";";
+        $set .= "\n\t}";
+        $set .= "\n\n";
+        $this->getSet .= $set;
+    }
+    #endregion
+
+    /**
+     * @param $referencia
+     * @details Cria o set passando para o atributo getSet
+     */
+    #region CreateSet
+    public function createSet($referencia)
+    {
+        $set = "\n\tpublic function set" . ucwords($referencia);
+        $set .= "($" . strtolower($referencia) . ")\n";
+        $set .= "\t{\n";
+        $set .= "\t\t$" . "this->" . strtolower($referencia) . " = $" . strtolower($referencia) . ";";
+        $set .= "\n\t}";
+        $set .= "\n\n";
+        $this->getSet .= $set;
+    }
+    #endregion
+
+    /**
+     * @param $referencia
+     * @details Cria o get passando para o atributo getSet
+     */
+    #region CreateGet
+    public function createGet($referencia)
+    {
+        $get = "\n\tpublic function get" . ucwords($referencia);
+        $get .= "()";
+        $get .= "\n\t{\n";
+        $get .= "\t\treturn $" . "this->" . strtolower($referencia) . ";";
+        $get .= "\n\t}";
+        $get .= "\n\n";
+        $this->getSet .= $get;
+    }
+    #endregion
+
+    /**
+     * @param $value
+     * @details Cria os atributos com referencias
+     */
+    #region CreateAtributoRef
+    public function createAtributoRef($value)
+    {
+        $this->atributos .= "\tprivate $" . $value . "; \n\t// References - " . $value ." \n\n";
+    }
+    #endregion
+
+    /**
+     * @param $value
+     * @param $type
+     * @details cria os atributos com os tipos de dados da coluna comentados
+     */
+    #region CreateAtribute
+    public function createAtribute($value, $type)
+    {
+        $this->atributos .= "\tprivate $" . $value . "; \n\t// " . $type ." \n\n";
+    }
+    #endregion
+
+    /**
+     * @param $value
+     * @details Cria os uses dos arquivos
+     */
+    #region CreateUse
+    public function createUse($value)
+    {
+        $this->use .= "\nuse MVC\\Model\\" . $value . ";";
+    }
+    #endregion
+}
--- SkyfallFramework/Kernel/Traits/ORM.trait.php
+++ SkyfallFramework/Kernel/Traits/ORM.trait.php
@@ -1,16 +1,29 @@
 <?php
 
 namespace  SkyfallFramework\Kernel\Traits;
-use SkyfallFramework\Common\Database\Connection;
 
+use SkyfallFramework\Common\Database\Connection;
+use SkyfallFramework\Common\ORM\ORMmodel;
+
+/**
+ * Trait ORM
+ * @package SkyfallFramework\Kernel\Traits
+ * @author Giovani Cassiano Nogueira <giovani.cassiano@outlook.com>
+ */
 trait ORM
 {
+    #region Atributos
     private $list_tables      = array();
     private $list_atributos   = array();
     private $list_foreign_key = array();
     private $list_primary_key = array();
     private $con;
+    #endregion
 
+    /**
+     * @author Giovani Cassiano Nogueira
+     */
+    #region Generate ORM
     public function generateORM()
     {
         $this->getTables();
@@ -19,7 +32,12 @@ trait ORM
         $this->createClassModel();
         $this->createController();
     }
+    #endregion
 
+    /**
+     * @details Recupera as tabelas
+     */
+    #region Get Tables
     public function getTables()
     {
         $this->con = Connection::$list_connection;
@@ -30,7 +48,12 @@ trait ORM
             $this->list_tables[] = $value['tabela'];
         }
     }
+    #endregions
 
+    /**
+     * @details Recupera as colunas
+     */
+    #region Get Columns
     public function getColumns()
     {
         foreach ($this->list_tables as $index => $value)
@@ -44,7 +67,12 @@ trait ORM
         }
         $this->getForeignKey();
     }
+    #endregions
 
+    /**
+     * @details Recupera as chaves estrangeiras das tabelas
+     */
+    #region Get Foreign Key
     public function getForeignKey()
     {
         foreach ($this->list_tables as $index => $value)
@@ -61,7 +89,12 @@ trait ORM
         }
         $this->getPrimaryKey();
     }
+    #endregions
 
+    /**
+     * @details Recupera as cháves primárias das tabelas
+     */
+    #region Get Primary Key
     public function getPrimaryKey()
     {
         foreach ($this->list_tables as $index => $value)
@@ -71,23 +104,21 @@ trait ORM
             $this->list_primary_key[$value] = $pes[0]['Column_name'];
         }
     }
+    #endregion
 
-    public function createHeader($type, $namespace, $uses)
-    {
-        $txt ="<?php\n/*\n /**\n Generation ORM Skyfall Framework\n**/";
-        return "";
-    }
-
+    /**
+     * @details Criar os arquivos de traits
+     */
+    #region Create Trait
     public function createTrait()
     {
         foreach ($this->list_tables as $index => $table)
         {
-            $extends = "";
-            $use = "";
-            $atributos = "";
-            $setGet = "";
             $model = "";
             $r = "";
+
+            $modelOrm = new ORMmodel();
+            $modelOrm->setTable(ucwords($table));
 
             if(key_exists($table,$this->list_foreign_key))
             {
@@ -95,95 +126,87 @@ trait ORM
                 {
                     if($value['referencia'] == $table)
                     {
-                        $model = " as " . strtolower($value['referencia']) . "Model";
+                        $model = " as " . ucwords($value['referencia']) . "Model";
                         $r = "Model";
                     }
-
-                    $use .= "\nuse MVC\\Model\\".ucwords($value['referencia']).$model.";";
-
-                    $atributos .= "  private $" . ucwords($value['referencia']) . "; \n  // References - "
-                                    .ucwords($value['referencia']) ." \n\n";
-
-                    $setGet .= "\n  public function set" . ucwords($value['referencia']) . "(" . strtolower($value['referencia']).$r
-                                . " $" . $value['referencia'].")\n  { \n" .
-                        "       $" . "this->" . ucwords($value['referencia']) .
-                        " = $" . $value['referencia'] . ";  \n  }\n";
-
-                    $setGet .= "\n  public function get" . ucwords($value['referencia']) . "()\n  { \n" .
-                        "       return $" . "this->" . ucwords($value['referencia']) . ";  \n  }\n";
+                    $modelOrm->createUse(ucwords($value['referencia']).$model);
+                    $modelOrm->createAtributoRef(ucwords($value['referencia']));
+                    $modelOrm->createSetRef($value['referencia'],$r);
+                    $modelOrm->createGet($value['referencia']);
                 }
             }
-
             foreach ($this->list_atributos[$table] as $i => $atributo)
             {
-                $atributos .= "  private $" . strtolower($atributo['Field']) . "; \n  // " .$atributo['Type'] ." \n\n";
-
-                $setGet .= "\n  public function set" . ucwords($atributo['Field']) .
-                    "($" .strtolower($atributo['Field']). ")\n  { \n" .
-                    "       $" . "this->" . strtolower($atributo['Field']) . " = $" . strtolower($atributo['Field'])
-                    . ";  \n  }\n";
-
-                $setGet .= "\n  public function get" . ucwords($atributo['Field']) . "()\n  { \n" .
-                    "       return $" . "this->" . strtolower($atributo['Field']) . ";  \n  }\n";
+                $modelOrm->createAtribute(strtolower($atributo['Field']), $atributo['Type']);
+                $modelOrm->createSet($atributo['Field']);
+                $modelOrm->createGet($atributo['Field']);
             }
-
-            $txt = "<?php\n\n/**\n Skyfall Micro-Framework\n Generate ORM Trait\n Version 1.0.0\n**/\n\nnamespace MVC\Traits; \n "
-                . $use . "\n\ntrait " . ucwords($table) ."\n{\n" . $atributos . $setGet . "\n}";
-
-            $path = dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR .
-                ".." . DIRECTORY_SEPARATOR . "MVC" . DIRECTORY_SEPARATOR . "Traits" . DIRECTORY_SEPARATOR;
-
-            $file = $path . ucwords($table) . ".trait.php";
-
-            $this->createFile($txt, $file);
+            $file = $this->path("Traits",ucwords($table));
+            $this->createFile($modelOrm->createFiles(), $file);
         }
     }
+    #endregion
 
+    /**
+     * @details Cria os arquivos models
+     */
+    #region Create Class Model
     public function createClassModel()
     {
         foreach ($this->list_tables as $index => $table)
         {
-            $extends = "\nuse SkyfallFramework\Common\CRUD\Model;";
-            $use = "\n  use \\MVC\\Traits\\".ucwords($table.";");
-            $setGet = "";
-
-            $txt = "<?php\n\n/**\n Skyfall Micro-Framework\n Generate ORM Class Model\n Version 1.0.0\n**/\n\nnamespace MVC\Model; \n "
-                . $extends ."\n\nclass " . ucwords($table) ." extends Model\n{" . $use . "\n}";
-
-            $path = dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR .
-                ".." . DIRECTORY_SEPARATOR . "MVC" . DIRECTORY_SEPARATOR . "Model" . DIRECTORY_SEPARATOR;
-
-            $file = $path . ucwords($table) . ".class.php";
-
+            $model = new ORMmodel();
+            $model->createUse(ucwords($table));
+            $file = $this->path("Model",ucwords($table));
             if(!file_exists($file))
-                $this->createFile($txt, $file);
+                $this->createFile($model->createModel(ucwords($table)), $file);
         }
     }
+    #endregion
 
+    /**
+     * @details Cria os arquivos de controllers
+     */
+    #region Create Controller
     public function createController()
     {
         foreach ($this->list_tables as $index => $table)
         {
-
-            $use = "\nuse MVC\\Model\\".ucwords($table)." as ".$table."Model;";
-
-            $txt = "<?php\n\n/**\n Skyfall Micro-Framework\n Generate ORM Controller\n Version 1.0.0\n**/\n\nnamespace MVC\Controller; \n "
-                . $use ."\n\nclass " . ucwords($table) ."\n{\n /**\n Skyfall Micro-Framework\n Body of Controller\n**/\n}";
-
-            $path = dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR .
-                ".." . DIRECTORY_SEPARATOR . "MVC" . DIRECTORY_SEPARATOR . "Controller" . DIRECTORY_SEPARATOR;
-
-            $file = $path . ucwords($table) . ".class.php";
-
+            $model = new ORMmodel();
+            $file = $this->path("Controller",ucwords($table));
             if(!file_exists($file))
-                $this->createFile($txt, $file);
+                $this->createFile($model->createController(ucwords($table)), $file);
         }
     }
+    #endregion
 
+    /**
+     * @param $textFinal
+     * @param $file
+     * @details Cria os arquivos em duas devidas pastas
+     */
+    #region Create File
     public function createFile($textFinal,$file){
         $myfile = \fopen($file, "w") or die("Unable to open file!");
         \fwrite($myfile, $textFinal);
         \fclose($myfile);
     }
+    #endregion
+
+    /**
+     * @param $type
+     * @param $table
+     * @return string
+     * @details retorna o caminho dos arquivos que vão ser gerados
+     */
+    #region Path
+    public function path($type, $table)
+    {
+        $path = dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR;
+        $path .= ".." . DIRECTORY_SEPARATOR . "MVC" . DIRECTORY_SEPARATOR . $type . DIRECTORY_SEPARATOR;
+        $path .= ucwords($table) . ".class.php";
+        return $path;
+    }
+    #endregion
 
 }
