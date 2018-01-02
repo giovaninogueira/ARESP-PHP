84265c49c5a766fad781d5e51bb848d784101a5d
Giovani Cassiano <giovani.cassiano@riosoft.com.br>
2018-01-02 15:35:00 -0200

    Nova estrutura do framework

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -1,25 +1,24 @@
 <?php
 
-namespace Core;
+namespace App;
 
 require_once __DIR__ .DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR .
     'SkyfallFramework' .DIRECTORY_SEPARATOR . 'App' .DIRECTORY_SEPARATOR
     .'Start.class.php';
 
-require_once __DIR__ . DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR .
-    'Config' . DIRECTORY_SEPARATOR . 'Config.class.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'App.class.php';
 
 use SkyfallFramework\App\Start;
-use Config\Config;
+use App\App;
 use SkyfallFramework\Kernel\Traits\Sessions;
 
 try{
-    $file_connection = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Config' . DIRECTORY_SEPARATOR . 'Database.ini';
+    $file_connection = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Database' . DIRECTORY_SEPARATOR . 'Database.ini';
     $obj = new Start();
     Sessions::validateSessionToken();
     $obj->load();
-    Config::$file_connection = $file_connection;
-    new Config();
+    App::$file_connection = $file_connection;
+    new App();
     $obj->run();
 }catch (\Exception $e){
     http_response_code($e->getCode());
--- App/generateORM.php
+++ App/generateORM.php
@@ -1,24 +1,23 @@
 <?php
 
-namespace Core;
+namespace App;
 
 require_once __DIR__ .DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR .
     'SkyfallFramework' .DIRECTORY_SEPARATOR . 'App' .DIRECTORY_SEPARATOR
     .'Start.class.php';
 
-require_once __DIR__ . DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR .
-    'Config' . DIRECTORY_SEPARATOR . 'Config.class.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'App.class.php';
 
 use SkyfallFramework\App\Start;
-use Config\Config;
+use App\App;
 use SkyfallFramework\Common\ORM\ORM;
 
 try{
-    $file_connection = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Config' . DIRECTORY_SEPARATOR . 'Database.ini';
+    $file_connection = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Database' . DIRECTORY_SEPARATOR . 'Database.ini';
     $obj = new Start();
     $obj->load();
-    Config::$file_connection = $file_connection;
-    new Config();
+    App::$file_connection = $file_connection;
+    new App();
     $orm = new ORM();
     $orm->generateORM();
 }catch (\Exception $e){
