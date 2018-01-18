31809053ace1f703a0acc092e015b3243ae95e5c
Giovani Cassiano <giovani.cassiano@riosoft.com.br>
2018-01-18 15:06:56 -0200

    Tabulação

--- App/Router.class.php
+++ App/Router.class.php
@@ -23,11 +23,44 @@ class Router
         $routes->addRoutes('GET','/Convenio/Lista','Convenio','getList',false);
         $routes->addRoutes('GET','/Cliente/:id','Cliente','getClienteById',false);
         $routes->addRoutes('GET','/Cliente/Lista','Cliente','getList',false);
-        $routes->addRoutes('POST','/Usuario/Login','Usuario','login',false,['email','senha']);
-        $routes->addRoutes('POST','/Convenio/Save','Convenio','save',false,['nome','telefone','obs']);
+        $routes->addRoutes('POST','/Usuario/Login','Usuario','login',false,
+            [
+                'email',
+                'senha']
+        );
+        $routes->addRoutes('POST','/Convenio/Save','Convenio','save',false,
+            [
+                'nome',
+                'telefone',
+                'obs']
+        );
         $routes->addRoutes('POST','/Empresa/Save','Empresa','save',false,
-            ['email','observacao','razao_social','nome_fantasia','cnpj','insc_municipal','insc_estadual','convenio_id']);
+            [
+                'email',
+                'observacao',
+                'razao_social',
+                'nome_fantasia',
+                'cnpj',
+                'insc_municipal',
+                'insc_estadual',
+                'convenio_id'
+            ]
+        );
         $routes->addRoutes('POST','/Cliente/Save','Cliente','Save',false,
-            ['nome','cpf','rg','nome_pai','update', 'id','nome_mae','data_nascimento','observacao','email','sexo','estado_civil','tipo_socio']);
+            [
+                'nome',
+                'cpf',
+                'rg',
+                'nome_pai',
+                'update',
+                'id',
+                'nome_mae',
+                'data_nascimento',
+                'observacao',
+                'email',
+                'sexo',
+                'estado_civil',
+                'tipo_socio']
+        );
     }
 }
