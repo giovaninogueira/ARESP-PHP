ce14c9d393629157dfab586260150cd9299c5ad1
Giovani Cassiano Nogueira <giovani.cassiano@outlook.com>
2018-06-30 17:00:16 -0300

    Correções de clientes

--- Data/Controller/Cliente.class.php
+++ Data/Controller/Cliente.class.php
@@ -158,12 +158,12 @@ class Cliente
         $banco = new \Data\Model\Banco();
 
         if($listDadoBancarios){
-            var_dump($listDadoBancarios);
-            die;
             if($listDadoBancarios['tipo'] == 'BOLETO'){
                 $obj["dadosBancarios"]['banco'] = new \stdClass();
                 $obj["dadosBancarios"]['operadora'] = new \stdClass();
                 $obj["dadosBancarios"]['tipo'] = 'BOLETO';
+                echo 'teste';
+                die;
             }else{
                 $banco->where('id','=',$listDadoBancarios["banco"]);
                 $listBanco = $banco->select();
