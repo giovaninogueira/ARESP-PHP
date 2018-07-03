3645b0c9c6d1b41bb9ee9dd09f42eb66c99e9a3a
Giovani Cassiano Nogueira <giovani.cassiano@outlook.com>
2018-07-02 22:27:34 -0300

    Correçõs

--- Data/Controller/Cliente.class.php
+++ Data/Controller/Cliente.class.php
@@ -114,8 +114,10 @@ class Cliente
                 $result = $cliente->select();
                 $listCliente = $this->find($result);
             }else{
-                $cliente->where('cancelamento_id','=','null');
+                $cliente->where('CANCELAMENTO_ID','=','null');
                 $listCliente = $cliente->select();
+                var_dump($listCliente);
+                die;
                 foreach ($listCliente as $index=>$obj){
                     $listCliente[$index] = $this->find($obj);
                 }
