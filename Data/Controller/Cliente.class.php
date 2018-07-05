b0c445f7fd9f7b7e1bae66ab2ece4e9bba7084da
Giovani Cassiano Nogueira <giovani.cassiano@outlook.com>
2018-07-05 20:20:13 -0300

    correções

--- Data/Controller/Cliente.class.php
+++ Data/Controller/Cliente.class.php
@@ -184,7 +184,8 @@ class Cliente
                 $obj["dadosBancarios"]['numeroCartao'] = $auxGrpNumCartao;
             }
             else if($listDadoBancarios['tipo'] == 'DEBITO'){
-
+                echo 'oi';
+                die;
                 $banco->where('id','=',$listDadoBancarios["banco"]);
                 $listBanco = $banco->select();
                 $listDadoBancarios["banco"] = ($listBanco) ? $listBanco: new \stdClass();;
